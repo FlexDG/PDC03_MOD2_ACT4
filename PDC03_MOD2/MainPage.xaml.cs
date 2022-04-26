096666c78018fd9c890fd6e350f8b38b494b9ad1
Felix Rafael De Guzman <deguzman.felixrafael@auf.edu.ph>
2022-04-26 22:26:26 +0800

    Add project files.

---
+++ PDC03_MOD2/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PDC03_MOD2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+
+            private async void Act1(object sender, EventArgs e)
+            {
+                await Navigation.PushAsync(new Page1());
+            }
+        }
+    }
+
